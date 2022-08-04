31013f46fba5dece5da1f18e566ba1d363876761
suryanarayanan22 <109964044+suryanarayanan22@users.noreply.github.com>
2022-08-04 12:53:43 +0530

    Create ActionsClass.java

---
+++ ActionsClass.java
@@ -0,0 +1,23 @@
+//Actions class user to perporm mouse over actions using selenium
+//Actions class method contains following method
+//1. clickAndHold() ==click (without releasing) at the current mouse location
+//2.contextClick()== rightclick
+//3.doubleClick()
+//dragAndDrop(source, target)
+//dragAndDropBy( source, X-offset, Y-offset)
+//moveToElement(toElement)== move the element from scource to target
+//release() == release the holded mouse
+
+//mouseover operation == to keep the mouse on the top of the element so it will show the drop down values that is called mouse over actions
+
+Actions act = new Actions(driver);
+WebElements  admin = driver.finElement("");
+WebElements  usermanagemt = driver.finElement("");
+WebElements  user = driver.finElement("");
+
+act.movetoElement(admin).perform(); //it will move the mouse to the specified location
+act.movetoElement(usermanagment).perform(); //it will move the mouse to the specified location
+act.movetoElement(user).click().perform(); // it will mouseover and click the the element
+//instant of above three codes we can also right like this
+act.movetoElement(admin).movetoElement(userman).movetoElement(user).perform();
+22.mins till
