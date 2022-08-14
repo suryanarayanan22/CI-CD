f8ef5583bcbd547780b9d0bcaca2e1b68b148137
suryanarayanan22 <109964044+suryanarayanan22@users.noreply.github.com>
2022-08-14 21:30:56 +0530

    Create Session3.java

---
+++ Java_newFutures/Session3.java
@@ -0,0 +1,2 @@
+//i can use lambda expresion, multi threading concept using runable interface that needs to be learned in detail by me in future 
+//i can also use lambda expresion in colloection conecpts that must also need to leared by me in more detail all though you know about list and array list
